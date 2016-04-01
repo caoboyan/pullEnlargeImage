3626f5a92f4e753ad3c24210d97902d6f9de47d3
boyancao <boyancao@meilishuo.com>
2016-04-01 11:40:00 +0800

    done

--- pullImageLarge/pullImageLarge/ViewController.m
+++ pullImageLarge/pullImageLarge/ViewController.m
@@ -1,27 +1,76 @@
 //
 //  ViewController.m
-//  pullImageLarge
+//  headerView
 //
-//  Created by Boyan Cao on 16/4/1.
+//  Created by Boyan Cao on 16/3/30.
 //  Copyright © 2016年 2015-293. All rights reserved.
 //
 
 #import "ViewController.h"
 
-@interface ViewController ()
+static CGFloat topSpacing = 300;
+
+@interface ViewController ()<UITableViewDataSource,UITableViewDelegate>
+
+@property (nonatomic, strong) UITableView * tableView;
+
+@property (nonatomic, strong) NSArray * dataList;
+
+@property (nonatomic, strong) UIView * headerView;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+-(NSArray *)dataList{
+    if (!_dataList) {
+        _dataList = [[NSArray alloc]init];
+    }
+    return _dataList;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.tableView = [[UITableView alloc]initWithFrame:self.view.bounds];
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    self.tableView.contentInset = UIEdgeInsetsMake(topSpacing, 0, 0, 0);
+    UIView * headerView = [[UIView alloc]initWithFrame:CGRectMake(0, -topSpacing , [UIScreen mainScreen].bounds.size.width, topSpacing)];
+    headerView.backgroundColor = [UIColor orangeColor];
+    self.headerView = headerView;
+    [self.tableView addSubview:self.headerView];
+    [self.view addSubview:self.tableView];
+    
+}
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 1;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cell"];
+    }
+    cell.textLabel.text = [NSString stringWithFormat:@"测试%i",indexPath.row];
+    return cell;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 10;
+}
+
+-(void)scrollViewDidScroll:(UIScrollView *)scrollView{
+    CGRect rect = self.headerView.frame;
+    rect.origin.y = scrollView.contentOffset.y;
+    rect.size.height = -scrollView.contentOffset.y;
+    self.headerView.frame = rect;
+    
+}
+
+
+-(void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate{
+    
 }
 
 @end
